2842ea84aa75469a47898df09f866d89034ba1b6
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-05-10 21:05:21 +0600

    navbar with tailwind implement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Pages/Shared/Navbar";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is doctor portal</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
